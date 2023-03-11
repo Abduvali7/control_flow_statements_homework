fb3d8f9faa648b09083dc1bd947416904db87005
Abduvali7 <hamzaqulovagulmira@gmail.com>
2023-03-11 10:44:26 +0500

    07

--- if07.py
+++ if07.py
@@ -14,11 +14,11 @@ def main(a):
     """
     if a>0 and a%2==1:
         return "positive odd number"
-    else: 
+    if a>0 and a%2==0:
        return "positive even number"
     if a<0 and a%2==1:
         return "negative odd number"
-    else :
+    if a<0 and a%2==0:
         return "negative even number"
     if a==0:
         return "the number is zero"
