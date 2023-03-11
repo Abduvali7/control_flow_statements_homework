9d083ff0b3080943da7acece42b90fa31d97dc1d
Abduvali7 <hamzaqulovagulmira@gmail.com>
2023-03-11 10:54:54 +0500

    08

--- if08.py
+++ if08.py
@@ -13,11 +13,11 @@ def main(a):
     """
     if a<9 and a>100 and a%2==1:
         return "two-digit odd number"
-    if a<9 and a>100 and a%2==0:
+    elif a<9 and a>100 and a%2==0:
         return "two-digit even number"
     if a<99 and a>1000 and a%2==1:
         return "three-digit odd number"
-    if a<99 and a>1000 and a%2==0:
+    elif a<99 and a>1000 and a%2==0:
         return "three-digit even number"
 print(main(2))
     
